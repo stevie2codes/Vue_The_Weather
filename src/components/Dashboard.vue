21e1b639a8c8b2c1b6428696b2f45269d851f435
stevie2codes <stevie2codes@gmail.com>
2020-08-01 17:05:59 -0500

    added background and hiding and showing inital data

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,7 +4,11 @@
       <input type="text" v-model="input" name="input" placeholder="Enter a location" />
       <button type="submit"></button>
     </form>
-    <h1>{{currentWeather}}</h1>
+    <h1 v-hide="recived ">Enter a Location</h1>
+    <div v-show="recieved" class="currentWrapper">
+      <h1 v-if="recieved">{{currentWeather.location.name}},{{currentWeather.location.region}}</h1>
+      <h3 v-if="recieved">{{currentWeather.current.temp_f}}degrees</h3>
+    </div>
   </div>
 </template>
 
@@ -15,24 +19,38 @@ export default {
   data() {
     return {
       currentWeather: {},
-      input: ""
+      input: "",
+      recieved: false
     };
   },
 
   methods: {
     fetchData: async function() {
-      const res = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=484a4fd44fb94f04b3a191835200108&q=${this.input}`
-      );
-      const currentWeather = await res.json();
-      this.currentWeather = currentWeather;
+      try {
+        const res = await fetch(
+          `http://api.weatherapi.com/v1/current.json?key=484a4fd44fb94f04b3a191835200108&q=${this.input}`
+        );
+        const currentWeather = await res.json();
+        this.currentWeather = currentWeather;
+      } catch (e) {
+        console.log(e);
+      }
     },
     processInput: function() {
       this.fetchData();
+      this.recieved = true;
     }
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+.currentWrapper {
+  background-color: white;
+  width: 500px;
+  margin: auto;
+  height: 600px;
+  opacity: 0.8;
+  border-radius: 10px;
+}
 </style>
