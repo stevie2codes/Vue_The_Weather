1b94d8b6a3dd7ddbd1ada71594fc346b5c7266ea
stevie2codes <stevie2codes@gmail.com>
2020-08-08 12:44:27 -0500

    redesign of search bar using bem and sass

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,44 +1,72 @@
 <template>
   <main>
     <div>
-      <form @submit.prevent="processInput">
-        <input type="text" v-model="input" name="input" placeholder="Search..." />
+      <form
+        @submit.prevent="processInput"
+        class="search-bar"
+        autocomplete="off"
+      >
+        <input
+          type="text"
+          v-model="input"
+          name="input"
+          placeholder="Search..."
+          class="search-bar__input"
+          aria-label="search"
+        />
+        <button class="search-bar__submit">
+          <i class="fas fa-search" aria-label="submit search"></i>
+        </button>
       </form>
-      <h1 class="title animate__animated animate__zoomInUp" v-if="!recieved">VUE The Weather</h1>
+      <h1 class="title animate__animated animate__zoomInUp" v-if="!recieved">
+        VUE The Weather
+      </h1>
       <div class="container">
         <div class="locDate">
           <h1 v-if="recieved" class="location">
-            {{location.
-            name}},{{location.region}}
+            {{ location.name }},{{ location.region }}
           </h1>
-          <div class="date">{{dateBuilder()}}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
 
-        <div v-if="recieved" class="currentWrapper animate__animated animate__slideInLeft">
-          <h3 v-if="recieved" class="temp">{{Math.round(weather.temp_f)}}&deg;</h3>
+        <div
+          v-if="recieved"
+          class="currentWrapper animate__animated animate__slideInLeft"
+        >
+          <h3 v-if="recieved" class="temp">
+            {{ Math.round(weather.temp_f) }}&deg;
+          </h3>
 
           <div class="dataWrap">
-            <h3 v-if="recieved" class="currentWeather">{{current.text}}</h3>
+            <h3 v-if="recieved" class="currentWeather">{{ current.text }}</h3>
             <div :style="styles" class="icon"></div>
           </div>
-          <h3 v-if="recieved" class="feels">Feels Like {{weather.feelslike_f}}&deg; F</h3>
-          <h3 v-if="recieved" class="feels">Humidity {{weather.humidity}}%</h3>
-          <h4 v-if="recieved" class="feels">Wind speed {{weather.wind_mph}} Mph</h4>
+          <h3 v-if="recieved" class="feels">
+            Feels Like {{ weather.feelslike_f }}&deg; F
+          </h3>
+          <h3 v-if="recieved" class="feels">
+            Humidity {{ weather.humidity }}%
+          </h3>
+          <h4 v-if="recieved" class="feels">
+            Wind speed {{ weather.wind_mph }} Mph
+          </h4>
         </div>
 
         <!-- Forecast -->
         <div class="forecastWrap">
-          <div v-for="(data,index) in forecast" :key="index" class="day">
-            <h4 class="dates">{{data.date}}</h4>
+          <div v-for="(data, index) in forecast" :key="index" class="day">
+            <h4 class="dates">{{ data.date }}</h4>
             <div :style="iconStyles" class="icon"></div>
             <div class="hiLowWrap">
-              <p class="hiTemp">Hi {{Math.round(data.day.maxtemp_f)}}&deg;</p>
-              <p class="lowTemp">Low {{Math.round(data.day.mintemp_f)}}&deg;</p>
+              <p class="hiTemp">Hi {{ Math.round(data.day.maxtemp_f) }}&deg;</p>
+              <p class="lowTemp">
+                Low {{ Math.round(data.day.mintemp_f) }}&deg;
+              </p>
             </div>
-            <p>{{data.day.condition.text}}</p>
-            <p>Chance of rain {{data.day.daily_chance_of_rain}}%</p>
-            <p>Sunrise {{data.astro.sunrise}}</p>
-            <p>Sunset {{data.astro.sunset}}</p>
+            <p>{{ data.day.condition.text }}</p>
+            <p>Chance of rain {{ data.day.daily_chance_of_rain }}%</p>
+            <p>Sunrise {{ data.astro.sunrise }}</p>
+            <p>Sunset {{ data.astro.sunset }}</p>
           </div>
         </div>
       </div>
@@ -140,7 +168,78 @@ export default {
 };
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+/* search styles */
+.fas {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.search-bar {
+  --size: 60px;
+  border: 2px solid tomato;
+  display: flex;
+  border-radius: 50px;
+  height: var(--size);
+  width: var(--size);
+  padding: 3px;
+  position: relative;
+  transition: 300ms cubic-bezier(0.18, 0.79, 0.2, 1.14);
+  overflow: hidden;
+  margin: 30px auto;
+
+  &__input {
+    flex-grow: 1;
+    font-size: 1.5rem;
+    padding: 0 0.5em;
+    border: 0;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    line-height: calc(var(--size) -3px);
+    cursor: pointer;
+    &:focus {
+      outline: 0;
+    }
+  }
+  &__submit {
+    font-size: 1.5rem;
+    cursor: pointer;
+    border: 0;
+    background: transparent;
+    border-radius: 50%;
+    width: calc(var(--size) - 10px);
+    height: calc(var(--size) - 10px);
+    margin-left: auto;
+    transition: background 150ms ease-in-out;
+  }
+  &:focus-within {
+    width: 100%;
+
+    .search-bar__input {
+      opacity: 1;
+      cursor: initial;
+      background: transparent;
+      width: calc(100% - var(--size));
+    }
+
+    .search-bar__submit {
+      background: tomato;
+      color: white;
+
+      &:focus,
+      &:hover {
+        outline: 0;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.35);
+      }
+    }
+  }
+}
+
 main {
   background-image: linear-gradient(
     to bottom,
@@ -216,44 +315,7 @@ main {
   width: 65px;
   height: 65px;
 }
-form {
-  width: 100%;
-  margin-bottom: 30px;
-  justify-content: center;
-  display: flex;
-}
-input {
-  width: 30vmax;
-  display: block;
-  padding: 10px;
-  color: #313131;
-  font-size: 20px;
-  border: none;
-  appearance: none;
-  outline: none;
-  background: none;
-  background-color: rgba(255, 255, 255, 0.25);
-  border-radius: 0px 16px 0px 16px;
-  transition: 0.4s;
-  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-}
-input::placeholder {
-  color: rgba(94, 83, 83, 0.79);
-  font-family: "Questrial", sans-serif;
-}
-input:focus,
-input:active {
-  width: 60vw;
-  outline: none;
-  box-shadow: none;
-  border-color: transparent;
-  background: none;
-  appearance: none;
-  background-color: rgba(255, 255, 255, 0.75);
-  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-  border-radius: 16px 0px 16px 0px;
-  color: rgba(0, 0, 0, 0.75);
-}
+
 .currentWrapper {
   background-color: rgba(0, 0, 0, 0.5);
   border-radius: 8px;
